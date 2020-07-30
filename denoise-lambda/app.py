1d884cb6467a0f59fe480afcb0b6056c62197ef8
sepehr ataei <s2ataei@ryerson.ca>
2020-07-29 21:58:49 -0400

    Upload to S3, return URLS

--- denoise-lambda/app.py
+++ denoise-lambda/app.py
@@ -9,19 +9,19 @@ def index():
     return {'status_code': 200,
             'message': 'welcome to test API'}
 
-@app.route('/upload/{file_name}', methods=['PUT'], content_types=['application/octet-stream'])
+@app.route('/upload/{file_name}', methods=['POST'], content_types=['image/png'], cors=True)
 def upload_to_s3(file_name):
     try:
         body = app.current_request.raw_body
         temp_file = '/tmp/' + file_name
         with open(temp_file, 'wb') as f:
             f.write(body)
-        s3.upload_file(temp_file, BUCKET, file_name)
-        return { 'file_name': file_name }
+        s3.upload_file(temp_file, 'imagebucket940127', file_name)
+        return { 'file_name': 'https://imagebucket940127.s3.ca-central-1.amazonaws.com/' + file_name }
     except Exception:
         raise ChaliceViewError('something went wrong')
 
-@app.on_s3_event(bucket=config.S3_UPLOADS_BUCKET_NAME,
+@app.on_s3_event(bucket='imagebucket940127',
 events=['s3:ObjectCreated:*'])
 def react_to_s3_upload(event):
     bucket = event['Bucket']
